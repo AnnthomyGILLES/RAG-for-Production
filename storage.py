241ba6bd9c119623660aa4e36c196990acbbc22f
AnnthomyGILLES <annthomy.gilles@gmail.com>
2024-01-06 00:51:44 -0500

    Improvements

--- storage.py
+++ storage.py
@@ -7,7 +7,7 @@ from loguru import logger
 import chromadb
 
 load_dotenv()
-# I've set this to our new embeddings model, this can be changed to the embedding model of your choice
+
 EMBEDDING_MODEL = "text-embedding-ada-002"
 
 embedding_function = OpenAIEmbeddingFunction(
@@ -16,7 +16,7 @@ embedding_function = OpenAIEmbeddingFunction(
 
 
 class StoreResults:
-    _instance = None  # Class variable to store the singleton instance
+    _instance = None
 
     def __init__(self):
         self.collection = self.chroma_client.get_or_create_collection(
@@ -34,20 +34,11 @@ class StoreResults:
 
     def __call__(self, batch):
         try:
-            # Prepare the data for insertion
-            embeddings, documents, metadatas, ids = [], [], [], []
+            documents = batch["text"].tolist()
+            embeddings = batch["embeddings"].tolist()
+            ids = batch["index"].tolist()
+            metadatas = [{"source": value} for value in batch["source"]]
 
-            for text, source, embedding, index in zip(
-                    batch["text"], batch["source"], batch["embeddings"], batch["index"]
-            ):
-                logger.info(f"{text} ----- {source} ------ .")
-                embeddings.append(embedding)
-                documents.append(text)
-                metadatas.append({"source": source})
-                ids.append(index)
-
-            # Upsert the data into the collection
-            # Assuming your collection's `add` method supports upserting
             self.collection.add(
                 embeddings=embeddings, documents=documents, metadatas=metadatas, ids=ids
             )
