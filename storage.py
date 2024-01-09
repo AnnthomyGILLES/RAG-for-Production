12ad7015f9c916970b3229417f02140c8c1b7dba
AnnthomyGILLES <annthomy.gilles@gmail.com>
2024-01-08 20:48:49 -0500

    Documenting

--- storage.py
+++ storage.py
@@ -1,4 +1,5 @@
 import os
+from typing import Any, Dict, List
 
 from chromadb.utils.embedding_functions import OpenAIEmbeddingFunction
 from dotenv import load_dotenv
@@ -16,6 +17,17 @@ embedding_function = OpenAIEmbeddingFunction(
 
 
 class StoreResults:
+    """
+    Singleton class for storing results in a ChromaDB collection.
+
+    This class is responsible for initializing and interacting with a ChromaDB client,
+    and provides functionality to add batches of documents to the ChromaDB collection.
+    It uses a singleton pattern to ensure only one instance manages the database connection.
+
+    Methods:
+        __call__(self, batch: Dict[str, Any]) -> Dict: Processes and adds a batch of documents to the collection.
+    """
+
     _instance = None
 
     def __init__(self):
@@ -32,12 +44,22 @@ class StoreResults:
             cls._instance.chroma_client = chromadb.PersistentClient(path="./chromadb")
         return cls._instance
 
-    def __call__(self, batch):
+    def __call__(self, batch: Dict[str, Any]) -> Dict:
+        """
+        Processes and adds a batch of documents to the collection.
+
+        Args:
+            batch (Dict[str, Any]): A batch of documents to be stored.
+                                    Expected keys are 'text', 'embeddings', 'index', and 'source'.
+
+        Returns:
+            Dict: An empty dictionary, potentially can be modified to return status or results.
+        """
         try:
-            documents = batch["text"].tolist()
-            embeddings = batch["embeddings"].tolist()
-            ids = batch["index"].tolist()
-            metadatas = [{"source": value} for value in batch["source"]]
+            documents: List[str] = batch["text"].tolist()
+            embeddings: List[Any] = batch["embeddings"].tolist()
+            ids: List[int] = batch["index"].tolist()
+            metadatas: List[Dict[str, str]] = [{"source": value} for value in batch["source"]]
 
             self.collection.add(
                 embeddings=embeddings, documents=documents, metadatas=metadatas, ids=ids
